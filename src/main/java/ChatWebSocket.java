77ff3d692ce9686c04be9f118811731264a29ecb
Manas Bajaj <manas.bajaj94@gmail.com>
2016-11-13 00:36:15 +0000

    Forgot to add chat websocket class in previous commit.

---
+++ src/main/java/ChatWebSocket.java
@@ -0,0 +1,30 @@
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by manasb on 12-11-2016.
+ */
+@WebSocket
+public class ChatWebSocket {
+
+    private final Chat chat = Chat.getInstance();
+    private static final Logger log = LoggerFactory.getLogger(ChatWebSocket.class);
+
+    @OnWebSocketConnect
+    public void onConnect(Session session) {
+        try {
+            chat.addUser(session);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    @OnWebSocketClose
+    public void onClose(Session session, int statusCode, String reason) {
+        chat.removeUser(session);
+    }
+}
