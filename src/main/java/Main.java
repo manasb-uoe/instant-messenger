37481aec8d601798320cef46e05ef6f4e6116c83
Manas Bajaj <manas.bajaj94@gmail.com>
2016-11-27 03:22:57 +0000

    Now static files are served from the Jetty server directly. Also added the ability to specify environment type as a command line arg when starting the server.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static spark.Spark.*;
 
 /**
@@ -6,9 +9,32 @@ import static spark.Spark.*;
 
 public class Main {
 
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        if (args.length == 0) {
+            throw new RuntimeException("Environment type must be provided");
+        }
+
+        Environment environment = Environment.valueOf(args[0]);
+
+        // If environment is DEV then static files are read from from an absolute path in order to
+        // refresh static files.
+        if (environment == Environment.DEV) {
+            String projectDir = System.getProperty("user.dir");
+            String staticDir = "\\src\\main\\resources\\public";
+            staticFiles.externalLocation(projectDir + staticDir);
+            log.info(projectDir + staticDir);
+        } else {
+            staticFiles.location("/public");
+        }
+
         webSocket("/chat", ChatWebSocket.class);
         init();
+    }
+
+    public enum Environment {
+        DEV, PROD
     }
 
 }
--- src/public/js/eventbus_client_events.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by manasb on 24-11-2016.
- */
-
-module.exports = Object.freeze({
-    SEND_NEW_MESSAGE: "send_new_message"
-});
