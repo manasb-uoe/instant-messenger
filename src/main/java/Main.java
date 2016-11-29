613eb2032b615e85b66f1fcd6699b69a39eff2dc
Manas Bajaj <manas.bajaj94@gmail.com>
2016-11-29 10:16:32 +0000

    Removed redundant cors crap + made a few vars final.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,14 +29,14 @@ public class Main {
 
         webSocket("/" + configReader.getWebSocketEndpoint(), ChatWebSocket.class);
         post("/edit-username", (request, response) -> {
-            String[] bodySplit = request.body().split(",");
+            final String[] bodySplit = request.body().split(",");
 
             if (bodySplit.length != 2) {
                 return HttpResponseFactory.createBadRequestResponse("Body must be in the format: existing_username,new_username");
             }
 
             try {
-                ChatController chatController = ChatController.getInstance();
+                final ChatController chatController = ChatController.getInstance();
                 final Session session = chatController.updateUsername(bodySplit[0], bodySplit[1]);
                 chatController.sendIdentityToSession(session);
                 chatController.broadcastConnectedUsers();
@@ -46,30 +46,4 @@ public class Main {
             }
         });
     }
-
-    // Enables CORS on requests. This method is an initialization method and should be called once.
-    private static void enableCORS(final String origin, final String methods, final String headers) {
-
-        options("/*", (request, response) -> {
-
-            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
-            if (accessControlRequestHeaders != null) {
-                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
-            }
-
-            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
-            if (accessControlRequestMethod != null) {
-                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
-            }
-
-            return "OK";
-        });
-
-        before((request, response) -> {
-            response.header("Access-Control-Allow-Origin", origin);
-            response.header("Access-Control-Request-Method", methods);
-            response.header("Access-Control-Allow-Headers", headers);
-        });
-    }
-
 }
